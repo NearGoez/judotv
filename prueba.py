82ff9d040106f23805927275f9b448a1f668ca21
NearGoez <Slythin07@gmail.com>
2025-10-21 01:36:35 -0300

    championship id getter uncompleted

--- prueba.py
+++ prueba.py
@@ -4,22 +4,33 @@ from bs4 import BeautifulSoup as bs
 from session_data import (cookies, headers)
 import json
 
-JUDO_TV_HOME_URL = "https://judotv.com/"
+JUDO_LIMA_GP_URL = "https://judotv.com/competitions/eju_u23_mda2025/overview"
 
-homepage_judotv_html = requests.get(JUDO_TV_HOME_URL, 
+championship_overview_html = requests.get(JUDO_LIMA_GP_URL, 
                                     cookies=cookies, headers=headers).text
 
-soup = bs(homepage_judotv_html, 'html.parser')
+soup = bs(championship_overview_html, 'html.parser')
 
-championship_name_div = soup.find(string=re.compile("3081"), recursive=True)
+championship_information_raw_json = soup.find('script', attrs={'data-nuxt-data': 'nuxt-app'})
 
-
-for i, node in enumerate(championship_name_div.parent):
-
-    with open(f'nodo{i}.json', 'w') as f:
-        f.write(node)
-
-    nodo_procesado = json.loads(node)
-    print(nodo_procesado)
+json_parsed = json.loads(championship_information_raw_json.text)
 
 
+svue_query_index = json_parsed[5]['$svue-query']
+
+queries_index = json_parsed[svue_query_index]['queries']
+
+nineth_index = json_parsed[queries_index][9]
+
+state_index = json_parsed[nineth_index]['state']
+
+data_index = json_parsed[state_index]['data']
+
+id_competition_index = json_parsed[data_index][0]
+
+id_competition = json_parsed[id_competition_index]
+name_competition = json_parsed[id_competition_index + 1]
+
+print(id_competition)
+print(name_competition)
+print(json_parsed[data_index])
